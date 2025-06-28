f9847a7bb318e6594f4c9dfe0ab8e9009e919705
adenijimuiz <muizodunayo111@gmail.com>
2025-06-28 20:15:34 +0100

    project str and custom hook

---
+++ src/hooks/useProduct.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+
+const useProduct = () => {
+  const [products, setProduct] = useState();
+  const [error, setError] = useState(null);
+
+  const fetchProducts = () => {
+    fetch('/src/products.json')
+      .then(res => res.json())
+      .then(data => setProduct(data))
+      .catch(err => setError(err))
+  };
+
+  useEffect(() => {
+    fetchProducts()
+  }, [products, error])
+
+  return {
+    products, error
+  }
+}
+
+export default useProduct
